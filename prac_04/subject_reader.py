6f071263e9279047777602c2f718ea88018c1a52
braedonross <jd111697@my.jcu.edu.au>
2024-10-11 10:20:40 +1000

    Refactor load data function to append to list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,20 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    """Read data from file formatted like: subject,lecturer,number of students, and append to list."""
+    data = []
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+            data.append(parts)
+    return data
 
 
 main()
