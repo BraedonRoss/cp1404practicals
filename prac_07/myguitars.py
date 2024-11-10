8f6f31086bc5fea97f62ac1605c4f8ed419d3778
braedonross <jd111697@my.jcu.edu.au>
2024-11-10 21:56:55 +1000

    Add function to read guitars from CSV file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,2 +1,15 @@
 from guitar import Guitar
 
+FILENAME = "Guitars.csv"
+
+def read_guitars(filename):
+    """Read guitars from a given file and return a list of Guitar objects."""
+    guitars = []
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            name = parts[0]
+            year = int(parts[1])
+            cost = float(parts[2])
+            guitars.append(Guitar(name, year, cost))
+    return guitars
