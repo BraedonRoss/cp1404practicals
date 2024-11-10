f6d3c6f39589ae9b0332e1c8ba7ea588faee966b
braedonross <jd111697@my.jcu.edu.au>
2024-11-10 22:00:26 +1000

    Add main and display functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,6 +2,12 @@ from guitar import Guitar
 
 FILENAME = "Guitars.csv"
 
+def main():
+    guitars = read_guitars(FILENAME)
+
+    print("Guitars read from file:")
+    display_guitars(guitars)
+
 def read_guitars(filename):
     """Read guitars from a given file and return a list of Guitar objects."""
     guitars = []
@@ -13,3 +19,9 @@ def read_guitars(filename):
             cost = float(parts[2])
             guitars.append(Guitar(name, year, cost))
     return guitars
+
+
+def display_guitars(guitars):
+    """Display a list of guitars."""
+    for guitar in guitars:
+        print(guitar)
