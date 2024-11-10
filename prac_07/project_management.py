ea23d1413a27d0152eeabb1281fc5d9d61fa285b
braedonross <jd111697@my.jcu.edu.au>
2024-11-10 23:35:58 +1000

    Add main function

---
+++ prac_07/project_management.py
@@ -0,0 +1,62 @@
+from project import Project
+
+
+FILENAME = "projects.txt"
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
+
+
+def main():
+    """Menu-driven program that allows users to manage projects."""
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects()
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != 'q':
+        if choice == 'l':
+            filename = input("Filename to load: ")
+            projects = load_projects(filename)
+        elif choice == 's':
+            filename = input("Filename to save: ")
+            save_projects(projects, filename)
+        elif choice == 'd':
+            display_projects(projects)
+        elif choice == 'f':
+            filter_date = input("Show projects that start after date (dd/mm/yyyy): ")
+            filter_projects_by_date(projects, filter_date)
+        elif choice == 'a':
+            add_project(projects)
+        elif choice == 'u':
+            update_project(projects)
+        else:
+            print("Invalid option, please choose again.")
+        print(MENU)
+        choice = input(">>> ").lower()
+
+    save_choice = input("Would you like to save to projects.txt? (y/n): ").lower()
+    if save_choice == 'y':
+        save_projects(projects)
+    print("Thank you for using custom-built project management software.")
+
+def load_projects():
+    pass
+
+
+def save_projects(projects, filename):
+    pass
+
+
+def display_projects(projects):
+    pass
+
+
+def filter_projects_by_date(projects, filter_date):
+    pass
+
+
+def update_project(projects):
+    pass
+
+
+def add_project(projects):
+    pass
+
