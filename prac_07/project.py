3e99bfcd39b52a4b863bfe99dee2f15b1e2cb780
braedonross <jd111697@my.jcu.edu.au>
2024-11-10 23:49:05 +1000

    Add docstrings for Project class methods

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,5 +1,7 @@
 class Project:
+    """Add Project class instance"""
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialize Project class instance"""
         self.name = name
         self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
@@ -7,21 +9,26 @@ class Project:
         self.completion_percentage = int(completion_percentage)
 
     def __str__(self):
+        """String representation of Project class instance"""
         return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
                 f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
                 f"completion: {self.completion_percentage}%")
 
     def __lt__(self, other):
+        """Less than operator for sorting projects"""
         return self.priority < other.priority
 
     def is_complete(self):
+        """Determine if project is complete"""
         return self.completion_percentage == 100
 
     def update(self, percentage=None, priority=None):
+        """Update percentage and priority of project"""
         if percentage is not None:
             self.completion_percentage = percentage
         if priority is not None:
             self.priority = priority
 
     def match_start_date(self, filter_date):
+        """Determine if project start date matches filter date"""
         return self.start_date > filter_date
