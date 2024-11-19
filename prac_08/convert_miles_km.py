adec0464f109c66a6a5ff8b395bb3fbb40ab4166
Braedonross <jd111697@my.jcu.edu.au>
2024-11-19 20:24:02 +1000

    Add validation function to conversion program

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,13 +11,20 @@ class MilesConverter(App):
         return self.root
 
     def handle_calculation(self):
-        value = float(self.root.ids.miles_input.text)
+        value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment_change(self, change):
-        value = float(self.root.ids.miles_input.text) + change
+        value = self.get_validated_miles() + change
         self.root.ids.miles_input.text = str(value)
         self.handle_calculation()
+
+    def get_validated_miles(self):
+        try:
+            value = float(self.root.ids.miles_input.text)
+            return value
+        except ValueError:
+            return 0
 
 MilesConverter().run()
