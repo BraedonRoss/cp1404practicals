caceaa85c9b5a8c44d6a8bc72d83e04e66598235
braedonross <jd111697@my.jcu.edu.au>
2024-11-24 19:57:56 +1000

    Add 'add' function to class

--- prac_09/band.py
+++ prac_09/band.py
@@ -9,3 +9,7 @@ class Band:
     def __str__(self):
         """Return a string representation of the Band."""
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
+
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
