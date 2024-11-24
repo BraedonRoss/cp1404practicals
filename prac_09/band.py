be97c337595bc5924df15e6f721974a9dd1a5ebe
braedonross <jd111697@my.jcu.edu.au>
2024-11-24 19:57:27 +1000

    Add band class with __init__ and __str__

---
+++ prac_09/band.py
@@ -0,0 +1,11 @@
+class Band:
+    """Band class to manage a collection of musicians."""
+
+    def __init__(self, name=""):
+        """Initialise a Band with a name and an empty list of musicians."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of the Band."""
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
