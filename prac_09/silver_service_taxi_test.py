c0cee97be45c1fcb92005fd43fd25ab4bf7f99b0
braedonross <jd111697@my.jcu.edu.au>
2024-11-24 19:44:17 +1000

    Update silver service taxi tests to round to .1

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -8,4 +8,4 @@ fare = hummer.get_fare()
 print(hummer)
 print(f"Fare for 18 km trip: ${fare:.2f}")
 
-assert round(fare, 2) == 48.78, f"Expected fare to be $48.78 but got ${fare:.2f}"
+assert round(fare, 1) == 48.8, f"Expected fare to be $48.8 but got ${fare:.1f}"
